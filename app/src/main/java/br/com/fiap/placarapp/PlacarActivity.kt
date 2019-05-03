b6c1a7c90332fa79b86726da0118cebf344abdf7
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-02 21:25:58 -0300

    first commit

---
+++ app/src/main/java/br/com/fiap/placarapp/PlacarActivity.kt
@@ -0,0 +1,46 @@
+package br.com.fiap.placarapp
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_placar.*
+
+class PlacarActivity : AppCompatActivity() {
+
+    private var golCasa = 0
+    private var golVisitante = 0
+
+    private lateinit var placarViewModel: PlacarViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placar)
+
+        placarViewModel = ViewModelProviders.of(this)
+                .get(PlacarViewModel::class.java)
+
+        placarViewModel.goalHome.observe(this, Observer {goalHome ->
+            tvPlacarCasa.text = "$goalHome"
+        })
+
+        placarViewModel.goalAway.observe(this, Observer {goalAway ->
+            tvPlacarVisitante.text = "$goalAway"
+        })
+
+        tvTimeCasa.text = intent.getStringExtra("TIME_CASA")
+        tvTimeVisitante.text = intent.getStringExtra("TIME_VISITANTE")
+
+        tvPlacarCasa.text = "${placarViewModel.goalHome}"
+        tvPlacarVisitante.text = "${placarViewModel.goalAway}"
+
+        btGolCasa.setOnClickListener {
+            placarViewModel.goalHome()
+        }
+
+        btGolVisitante.setOnClickListener {
+            placarViewModel.goalAway()
+        }
+
+    }
+}
